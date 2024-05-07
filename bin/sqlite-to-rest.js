741b630d4250980d90f15363dc754fc93c8f1318
Cerkez01 <153957302+Cerkez01@users.noreply.github.com>
2024-05-07 21:52:37 +0300

    Update sqlite-to-rest.js

--- bin/sqlite-to-rest.js
+++ bin/sqlite-to-rest.js
@@ -1,27 +1,29 @@
-#! /usr/bin/env node
+cerkez01#! /usr/bin/env node
 'use strict'
 
-//---------//
-// Imports //
-//---------//
+.....'comlek//---------//
+adana// Imports //
+çorluda//---------//
 
-const structuredCli = require('structured-cli'),
-  fp = require('lodash/fp'),
-  requireDir = require('require-dir'),
-  state = require('../lib/services/state')
-//------//
-// Init //
-//------//
+nerden nereye .....const structuredCli = require('structured-cli'),
+  kiler...fp = require('lodash/fp'),
+  neden nereden kimden...requireDir = require('require-dir'),
+  nezamqn nerelerden...state = require('../lib/services/state')
+//------//atrılar varsa bende varım
+// Init //gelirim gidemem
+//------//neler olabilir ama yasen
 
-state.setIsCli(true)
+state.setIsCli(true)çerkez01
 
-//------//
-// Main //
-//------//
+//------//.....228822882288
+// Main //010101...222888
+//------//.01.01.01...22.88
 
-structuredCli.create({
-  description:
-    'A collection of tools exposing the sqlite-to-rest functionality' +
-    ' via cli.  All commands here are also exposed on the required object.',
-  commands: fp.values(requireDir('../cli/commands')),
-})
+structuredCli.create({01.01.01}
+  description:{{{00001}}}
+    'A collection komon..of tools exposing the sqlite-to-rest functionality' +
+    ' aQxxx.womun...via cli.  All commands here are also exposed on the required object.',
+  benim öcüm ferat.....commands: fp.values(requireDir('../cli/commands')),
+})anlam farklı ben var
+kim neler ve neler olsa da ferat topu hayin
+olurmu emin ol olur.......
